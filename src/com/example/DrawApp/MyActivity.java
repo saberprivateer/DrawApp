a99026b159210dd1cbdd7430fa89f9c5fbf73fc4
Daniel Kamerling <dkamerling@riotgames.com>
2014-06-16 00:13:27 -0700

    Added a menu with sample share intent and case statement for clicking the menu.

--- src/com/example/DrawApp/MyActivity.java
+++ src/com/example/DrawApp/MyActivity.java
@@ -1,15 +1,68 @@
 package com.example.DrawApp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    private ShareActionProvider mShareActionProvider;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.menu_share);
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+
+        //placeholder intent until I can figure out how to grab the image
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        setShareIntent(sendIntent);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.menu_new_picture:
+                TextView tv = (TextView) findViewById(R.id.textView);
+                if (tv.getText() == "Pull Request Test") {
+                    tv.setText("Click the Menu");
+                } else {
+                    tv.setText("Pull Request Test");
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 }
